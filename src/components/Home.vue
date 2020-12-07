95f58d5720667e9d99edffcb95ed02014975bb93
ronaldo0010 <rcedras0010@gmail.com>
2020-12-07 18:12:14 +0200

    added a navBar component and home text

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <p id="intro" class="container">
+        As the first Developer Student Club in South Africa, DSC UCT prides
+        itself on preparing student developers for the real world, in the best
+        way possible. We teach people how to code, and turn coders into
+        developers by exposing them to the tools and technologies used in
+        industry. Our developers then become drivers of change, as they use
+        what they have learnt to build tech solutions to local and global
+        issues.
+    </p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="primary">
+      <b-navbar-brand href="#">Developer Student Club UCT</b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item >Events</b-nav-item>
+        <b-nav-item>Mentors</b-nav-item>
+        <b-nav-item>Projects</b-nav-item>
+        <b-nav-item>About</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+    
+  </div>
+</template>
+
+<script>
+import { BNavbar} from 'bootstrap-vue'
+
+    export default {
+    name: 'NavBar',
+    components: {
+      BNavbar, 
+    }
+}
+</script>
+
+<style scoped>
+  
+</style>
